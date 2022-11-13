8297260f0b3883c93efd1abea9b17348d9444387
Jose Ricardo Kremer Polesi <josericardo.kremer@gmail.com>
2022-11-12 21:25:19 -0300

    initial commit

--- lambda/index.js
+++ lambda/index.js
@@ -10,7 +10,7 @@ const LaunchRequestHandler = {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
     },
     handle(handlerInput) {
-        const speakOutput = 'Welcome, you can say Hello or Help. Which would you like to try?';
+        const speakOutput = 'Opaaa, preparado para um papo cabeça?';
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
