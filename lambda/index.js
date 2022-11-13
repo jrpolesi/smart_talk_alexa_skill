c59d22502d821f0c82e97fcad787562be7f0dfa6
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-11-13 01:29:11 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -112,7 +112,7 @@ const IntentReflectorHandler = {
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .reprompt('add a reprompt if you want to keep the session open for the user to respond')
             .getResponse();
     }
 };
